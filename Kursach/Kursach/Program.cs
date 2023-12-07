dcdd3de22e0ddce06f9350fb2dfed6025c1d9d49
ImVolcano <maks_kasimov@internet.ru>
2023-12-07 10:22:11 +0300

    Tested DB connection and editing in Program class

--- Kursach/Kursach/Program.cs
+++ Kursach/Kursach/Program.cs
@@ -1,5 +1,32 @@
-﻿SQLDBConnector connector = new SQLDBConnector("Server=(localdb)\\mssqllocaldb;Database=master;Trusted_Connection=True;");
+﻿using Microsoft.Data.SqlClient;
 
-if(connector.connectToDB()) Console.WriteLine("+");
+string connectionString = "Server=(localdb)\\mssqllocaldb;Database=bank;Trusted_Connection=True;";
+string sqlExpression = "SELECT * FROM Clients";
 
-if(connector.disconnectFromDB()) Console.WriteLine("+");
+using(SqlConnection conn = new SqlConnection(connectionString))
+{
+    conn.Open();
+
+    SqlCommand cmd = new SqlCommand(sqlExpression, conn);
+    SqlDataReader reader = cmd.ExecuteReader();
+
+    if (reader.HasRows)
+    {
+        string column0 = reader.GetName(0);
+        string column1 = reader.GetName(1);
+        string column2 = reader.GetName(2);
+        string column3 = reader.GetName(3);
+
+        Console.WriteLine($"{column0}\t{column1}\t{column2}\t{column3}");
+
+        while (reader.Read())
+        {
+            object id = reader.GetValue(0);
+            object name = reader.GetValue(1);
+            object surname = reader.GetValue(2);
+            object age = reader.GetValue(3);
+
+            Console.WriteLine($"{id}\t{name}\t{surname}\t{age}");
+        }
+    }
+}
--- Kursach/Kursach/obj/Debug/net8.0/Kursach.AssemblyInfo.cs
+++ Kursach/Kursach/obj/Debug/net8.0/Kursach.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kursach")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6585c81e79244134808962e907ea093fbdae9bc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c9e1a8596d099174aa8f256c40dad781f7e1c52")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kursach")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kursach")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
